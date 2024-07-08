34d992f39493fdfefd2b72dbfed15e8fe51f0491
tunahan <ttezcan.1999@gmail.com>
2024-07-08 12:32:10 +0300

    feat: Add Page component for readme
    A new Page component was added to the app/readme directory. This component renders a simple "page" div and can be used to display content on the readme page. This addition enhances the structure and organization of the readme page.
    
    Note: Please remove any unnecessary details and meta information from the commit message before finalizing it.

--- components/todos/atoms/search.tsx
+++ components/todos/atoms/search.tsx
@@ -1,16 +1,33 @@
+"use client";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import React, { useMemo } from "react";
 
 export default function Search({
   setSearch,
 }: {
   setSearch: (value: string) => void;
 }) {
+  const debouncedSetSearch = useMemo(
+    () => debounce(setSearch, 1000),
+    [setSearch],
+  );
   return (
     <Input
       className=""
       placeholder="Search todos"
-      onChange={(e) => setSearch(e.target.value)}
+      onChange={(e) => debouncedSetSearch(e.target.value)}
     />
   );
+}
+
+function debounce(func: (...args: any[]) => void, wait: number) {
+  let timeout: NodeJS.Timeout;
+  return function executedFunction(...args: any[]) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
 }
